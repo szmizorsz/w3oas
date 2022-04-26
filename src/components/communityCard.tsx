b9b32313dc31d4b9394e374c7300a0906c2b4fa2
Szentes Szabolcs <szentes.szabolcs@sonrisa.hu>
2022-04-26 14:55:53 +0200

    Communitiy icon display

--- src/components/communityCard.tsx
+++ src/components/communityCard.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
-import { Box, Text, Heading, Grid, GridItem, Center } from '@chakra-ui/react'
+import {
+  Box,
+  Text,
+  Heading,
+  Grid,
+  GridItem,
+  Center,
+  Image,
+} from '@chakra-ui/react'
 
 import type { CommunityFieldsFragment } from '../../graphql/generated/graphql'
 
@@ -23,37 +31,50 @@ export default function CommunityCard({ community }: Props) {
       >
         <Box as="a" mb="40">
           <Grid templateColumns="repeat(10, 1fr)" gap={6}>
-            <GridItem colSpan={8}>
-              <Heading m="5" mb="0" mr="0" as="h4" size="md">
-                {community.name} - owner: {community.owner.discord_user_name}
-              </Heading>
-            </GridItem>
-            <GridItem colSpan={2}>
-              <Center>
-                <Box
-                  borderWidth="1px"
-                  borderRadius="xl"
-                  background="#A0AEC0"
-                  my="3"
+            <Center>
+              <GridItem colSpan={1}>
+                {community.icon && (
+                  <Image ml={3} src={community.icon} alt="icon"></Image>
+                )}
+              </GridItem>
+            </Center>
+
+            <GridItem colSpan={9}>
+              <Grid templateColumns="repeat(10, 1fr)" gap={6}>
+                <GridItem colSpan={8}>
+                  <Heading m="5" mb="0" mr="0" as="h4" size="md">
+                    {community.name} - owner:{' '}
+                    {community.owner.discord_user_name}
+                  </Heading>
+                </GridItem>
+                <GridItem colSpan={2}>
+                  <Center>
+                    <Box
+                      borderWidth="1px"
+                      borderRadius="xl"
+                      background="#A0AEC0"
+                      my="3"
+                    >
+                      <Text color="white" px="2">
+                        126 w3oas members
+                      </Text>
+                    </Box>
+                  </Center>
+                </GridItem>
+              </Grid>
+              <Box h="40px">
+                <Text
+                  m="5"
+                  mt="2"
+                  overflow="hidden"
+                  whiteSpace="nowrap"
+                  textOverflow="ellipsis"
                 >
-                  <Text color="white" px="2">
-                    126 w3oas members
-                  </Text>
-                </Box>
-              </Center>
+                  {community.description}
+                </Text>
+              </Box>
             </GridItem>
           </Grid>
-          <Box h="40px">
-            <Text
-              m="5"
-              mt="2"
-              overflow="hidden"
-              whiteSpace="nowrap"
-              textOverflow="ellipsis"
-            >
-              {community.description}
-            </Text>
-          </Box>
         </Box>
       </Box>
     </Link>
